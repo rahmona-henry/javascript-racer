eff272054f2188e7ae66dbc553e79fb525ec1a12
Someone Special <someone@devacademy.co.nz>
2016-01-17 14:34:48 +1300

    Add track lenght alert

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ var lenght = prompt("Hello Racers,please choose your racetrack between: 5 and 15
 
 if(lenght>5)
 {
-  alert("Racetrack lenght must be greater than /
-   5 lenghts and less than or equal to 15./
-   Please choose again");
+  alert("Racetrack lenght must be greater than 5 lenghts and less than or equal to 15 please choose again");
 }
+
+//else
 //Check if racetrack must be between 5 and 15 lenghts
 
 //Create racetrack based on users input
-function makeTable(){
+//function makeTable(){
 //Write a for loop to create the number of rows <td> specified in the "row" input field
-for 
+//for 
