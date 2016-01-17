8438f816a4c96ac12a5d721729c6335c26655c93
Someone Special <someone@devacademy.co.nz>
2016-01-17 15:59:28 +1300

    Add track lenght for loop

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
+//To prevent any JQuery running before the document has finished loading
+$(document).ready(function(){
+
+
 
 //Users enter race track lenght
 var lenght = prompt("Hello Racers,please choose your racetrack between: 5 and 15 lenghts " );
 //Check if racetrack must be between 5 and 15 lenghts
 if(lenght<5)
 {
-  alert("Racetrack lenght must be lbetween 5 to 15 please choose again");
+  alert("Racetrack lenght must be between 5 to 15 please choose again");
 }
 
 else if (lenght>15)
 {
-  alert("Racetrack lenght must be lbetween 5 to 15 please choose again");
+  alert("Racetrack lenght must be between 5 to 15 please choose again");
 };
 
 //Create racetrack based on users input
-//function makeTable(){
-//Write a for loop to create the number of rows <td> specified in the "row" input field
-//for 
+
+    for (i = 1; i <= length; i++) {
+      console.log(i);
+      $(".racetrack").append("td");
+};
+     
+
+});
