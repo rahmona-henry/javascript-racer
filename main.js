2f74aeab039c17c8fbf6199b86e41af5dd8d6b3a
Someone Special <someone@devacademy.co.nz>
2016-01-17 16:43:37 +1300

    Fix length spelling

--- main.js
+++ main.js
@@ -4,21 +4,21 @@
 
 
 //Users enter race track lenght
-var lenght = prompt("Hello Racers,please choose your racetrack between: 5 and 15 lenghts " );
+var trackLength = prompt("Hello Racers,please choose your racetrack between: 5 and 15 lenghts " );
 //Check if racetrack must be between 5 and 15 lenghts
-if(lenght<5)
+if(trackLength<5)
 {
   alert("Racetrack lenght must be between 5 to 15 please choose again");
 }
 
-else if (lenght>15)
+else if (trackLength>15)
 {
   alert("Racetrack lenght must be between 5 to 15 please choose again");
 };
 
 //Create racetrack based on users input
 
-    for (i = 1; i <= lenght; i++) {
+    for (i = 1; i <= trackLength; i++) {
       console.log(i);
       $(".track").append("<td>");
     };
