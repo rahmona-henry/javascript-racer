c677c97ccf6bd11afc96b3477785422503805d33
Someone Special <someone@devacademy.co.nz>
2016-01-19 00:53:18 +1300

    Add event listener and updateplayerposition function

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-
 $( document ).ready(function() {
     console.log( "ready!" );
 
-
-
 //Users enter race track lenght
 var trackLength = prompt("Hello Racers,please choose your racetrack between: 5 and 15 lenghts " );
-//Check if racetrack must be between 5 and 15 lenghts
+
+//Check if racetrack is between 5 and 15 lenghts if not issue warning
 if(trackLength<5)
 {
   alert("Racetrack lenght must be between 5 to 15 please choose again");
@@ -17,12 +15,27 @@ else if (trackLength>15)
   alert("Racetrack lenght must be between 5 to 15 please choose again");
 };
 
-//Create racetrack based on users input
+//Create racetrack lenght based on users input
 
-    for (i = 1; i <= trackLength; i++) {
+    for (var i = 1; i <= trackLength; i++) {
       console.log(i);
       $(".track").append("<td>");
     };
+//add event listener for key pressed by users
+window.addEventListener("keyup", updatePlayerPosition, false);{
+  function updatePlayerPosition(e) {
+       if(e.keyCode == "81") {
+       $("Player1").addClass("blue");
+     }
+
+      else {
+        if(e.keyCode == "80") {
+        $("Player2").addClass("orange");
+      }
+
+}
+ }
 
 
 });
+
