7f3bde64121254bc134878f8e1394754bf424b32
Someone Special <someone@devacademy.co.nz>
2016-01-19 01:59:35 +1300

    add on

--- main.js
+++ main.js
@@ -15,15 +15,17 @@ else if (trackLength>15)
   alert("Racetrack lenght must be between 5 to 15 please choose again");
 };
 
-//Create racetrack lenght based on users input
+//Create racetrack length based on users input
 
     for (var i = 1; i <= trackLength; i++) {
       console.log(i);
       $(".track").append("<td>");
     };
+
 //add event listener for key pressed by users
-document.body.addEventListener("keyup", updatePlayerPosition, false);
-  function updatePlayerPosition(e) {
+ $(document).on('keyup', updatePlayerPosition)
+
+  function updatePlayerPosition {
        if(e.keyCode == "81") {
        $("#Player1").addClass("blue");
      }
