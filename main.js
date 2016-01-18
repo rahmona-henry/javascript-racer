ba22d6e247e2ee36d09e04d1acc6601e6eed242b
Someone Special <someone@devacademy.co.nz>
2016-01-19 03:14:39 +1300

    Add index

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 $('document').ready(function() {
- 
+    console.log( "ready!" );
 
 //Users enter race track lenght
-var trackLength = prompt("Hello Racers,please choose your racetrack between: 5 and 15 lenghts");
+var trackLength = prompt("Hello Racers,please choose your racetrack between: 5 and 15 lenghts " );
 
 //Check if racetrack is between 5 and 15 lenghts if not issue warning
 if(trackLength<5)
@@ -15,33 +15,27 @@ else if (trackLength>15)
   alert("Racetrack lenght must be between 5 to 15 please choose again");
 };
 
-//Create racetrack lenght based on users input
+//Create racetrack length based on users input
 
     for (var i = 1; i <= trackLength; i++) {
       console.log(i);
-      $(".racer_table").append("<td>");
+      $(".track").append("<td>");
     };
 
-
-//add event listener for keys pressed by players
-
-$(document).on('keyup',function updatePlayerPosition){
+//add event listener for key pressed by users
+ $(document).on('keyup',updatePlayerPosition)
 
   function updatePlayerPosition(e) {
 
-       if(e.keyCode === "81") {
-       $(".active").addClass("blue");
-        }
+       if(e.keyCode == "81") {
+       $("#Player1").addClass("blue");
+     }
 
       //if(e.keyCode == "80") 
       else {
         $("#Player2").addClass("orange");
-      }
-}
-}
+ }      
 
 
 
 });
-
-
