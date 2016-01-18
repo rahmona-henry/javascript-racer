66af9b6492ae54e6ba876a0b2fa40a0726e0f135
Someone Special <someone@devacademy.co.nz>
2016-01-19 09:52:33 +1300

    Add bracket

--- main.js
+++ main.js
@@ -21,16 +21,16 @@ else if (trackLength>15)
     };
 
 //Add EventListener that listens for users pushing their player keys and makes their cell active
-$(document).on("keyup","<td>",updatePlayerPosition){
+//$(document).on("keyup","<td>",updatePlayerPosition){
 
-  function updatePlayerPosition(e) {
-       if(e.keyCode == "81") {
-       $("<td>").addClass("black");
-     }
-      else if(e.keyCode == "80") {
-      $("<td>").addClass("black");
-    }       
-};
-};
+  //function updatePlayerPosition(e) {
+       //if(e.keyCode == "81") {
+      //$("<td>").addClass("black");
+     //}
+      //else if(e.keyCode == "80") {
+      //$("<td>").addClass("black");
+   //}       
+//};
+//};
 
 });
