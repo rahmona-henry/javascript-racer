1b1e97d17d52fa0174b796af3f061a0ae281c078
Someone Special <someone@devacademy.co.nz>
2016-01-19 09:51:15 +1300

    Add {

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 $('document').ready(function() {
-//Players enter racetrack length
 
+//Players enter racetrack length
 var trackLength = prompt("Hello Racers,please choose your racetrack length between:5 and 15 lenghts");
 
 //Check if racetrack is between 5 and 15 lenghts if not issue warning
@@ -21,7 +21,7 @@ else if (trackLength>15)
     };
 
 //Add EventListener that listens for users pushing their player keys and makes their cell active
-$(document).on("keyup","<td>",updatePlayerPosition)
+$(document).on("keyup","<td>",updatePlayerPosition){
 
   function updatePlayerPosition(e) {
        if(e.keyCode == "81") {
@@ -30,6 +30,7 @@ $(document).on("keyup","<td>",updatePlayerPosition)
       else if(e.keyCode == "80") {
       $("<td>").addClass("black");
     }       
-
+};
+};
 
 });
