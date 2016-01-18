e24301f26b0325788127bb5cc8dfc3438fd678ae
Someone Special <someone@devacademy.co.nz>
2016-01-19 09:43:06 +1300

    Add active

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ else if (trackLength>15)
     };
 
 //Add EventListener that listens for users pushing their player keys and makes their cell active
-$(document).on("keyup",".track",updatePlayerPosition){
+$(document).on("keyup","<td>",updatePlayerPosition){
 
   function updatePlayerPosition(e) {
        if(e.keyCode == "81") {
