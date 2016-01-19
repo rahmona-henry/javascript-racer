14e90f30bd211073124c4d85c5897b2364afa49e
Someone Special <someone@devacademy.co.nz>
2016-01-19 13:36:38 +1300

    Add js

--- main.js
+++ main.js
@@ -1,5 +1,27 @@
 $('document').ready(function() {
 
-  
+ var p1Moves = 0;
+ var p2Moves = 0; 
+ var tracklength = 8;
 
-});
+ $(document).on('keyup', function(keyID) { 
+
+ if (keyID.keyCode === 80){ 
+   if (p1Moves<trackLength) {
+     updatePlayer('player1_strip');
+     p1Moves++;
+     }
+   //else {
+    // win("P1");
+   //} 
+ }
+ else if (keyID.keyCode === 81) {
+   if (p2Moves<trackLength) {
+     updatePlayer('player2_strip');
+     p2Moves++;
+     }
+  // else {
+   //  win("P2");
+  //} 
+ }   
+} ) ;      
