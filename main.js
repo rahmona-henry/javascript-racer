bfc566936292b37e1f2f03dd4b56a63e0faff782
Someone Special <someone@devacademy.co.nz>
2016-01-20 16:12:33 +1300

    Trying a fix to make cells active wen key is pushed

--- main.js
+++ main.js
@@ -47,6 +47,10 @@ $("#player2_strip td").removeClass("active");
 
   p1Moves = 0;
   p2Moves = 0;
+
+ $("#player1_strip td:nth-child(2)").addClass("active");
+ $("#player2_strip td:nth-child(2)").addClass("active");
+
  } 
 });                                              
   
