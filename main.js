7636ad2e11f9eafc67cc6bd8ff6de57e4fb5f55c
Someone Special <someone@devacademy.co.nz>
2016-01-21 15:49:07 +1300

    Add normalize.css and correct spelling mistake in update plater function

--- main.js
+++ main.js
@@ -10,26 +10,26 @@ $(document).on('keyup', function(keyID) {
 
  if(keyID.keyCode === 80){ 
    if(p1Moves<trackLength) {
-     updatePlayer('player1_strip');        
-     p1Moves++;
+      updatePlayer('player1_strip');        
+      p1Moves++;
      } 
       else {
-      win("P1")
+      win("P1");
      }
    }
 
  else if(keyID.keyCode === 81) {
-   if (p2Moves<trackLength) {
-     updatePlayer('player2_strip');
-     p2Moves++;
+   if(p2Moves<trackLength) {
+      updatePlayer('player2_strip');
+      p2Moves++;
      }
       else {
-      win("P2")
+      win("P2");
      } 
  }  
 });
 
-var updatePLayer = function (player) {  
+var updatePlayer = function(player) {  
 
  var activeCell = $("#" + player + "td.active");
  var moveCell = activeCell.next();
@@ -38,16 +38,8 @@ var updatePLayer = function (player) {
  moveCell.addClass("active");
 };
 
-function win(winner){
-  alert(winner + "wins");
-
-  $("#player1_strip td").removeClass("active");   
-  $("#player2_strip td").removeClass("active");
-  
-  $("#player1_strip td:nth-child(2)").addClass("active");
-  $("#player2_strip td:nth-child(2)").addClass("active");
-
-
+function win(winner) {
+  alert(winner+" wins");
  
 }
 });                                              
