2bf6c3f0871c67be54d3b079e4d11920820ee08d
Someone Special <someone@devacademy.co.nz>
2016-01-21 16:09:05 +1300

    Replace listener

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-
 $(document).ready(function(){
 
 var p1Moves = 0;
 var p2Moves = 0;
 var trackLength = 0;
 
-function set () {                                                       //Function to let user set track length and build it.
-    
+function set () {                                                      
   trackLength = prompt("P1 use 'p' to move. \nP2 use 'q' to move.", 
                         "Set a track length. MAX 30");
     
@@ -22,29 +20,27 @@ function set () {                                                       //Functi
 }
 
 set();
-//Keyup Event Listener
-$(document).on('keyup', function(keyID) {
+$(document).on('keyup', function(keyID) {     
 
- if(keyID.keyCode === 80){ 
-   if(p1Moves<trackLength) {
-      updatePlayer('player1_strip');        
-      p1Moves++;
-     } 
-      else {
-      win("P1");
-     }
-   }
-
- else if(keyID.keyCode === 81) {
-   if(p2Moves<trackLength) {
+  if (keyID.keyCode === 80){                 
+    if (p1Moves<trackLength) {                
+      updatePlayer('player1_strip');          
+      p1Moves++;                              
+    }
+    else {
+      win("P1");                              
+    } 
+  }
+  else if (keyID.keyCode === 81) {
+    if (p2Moves<trackLength) {
       updatePlayer('player2_strip');
       p2Moves++;
-     }
-      else {
+    }
+    else {
       win("P2");
-     } 
- }  
-});
+    } 
+  }   
+});  
 
 var updatePlayer = function(player) {  
 
