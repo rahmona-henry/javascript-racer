90efb0bb02d77876001d9d59969b35ee0a7bec73
rahmona-henry <someone@devacademy.co.nz>
2016-02-08 22:56:21 +1300

    Try td_active

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ $(document).ready(function() {
     return '#' + Math.random().toString(16).slice(2, 8);
   }
 
-  $('keyID').on('keyup', function () {
+  $('td.active').on('keyup', function () {
     $('player2_strip').css('background-color', randomColour());
   })
 randomColour();
