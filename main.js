53c7a3abef49d2a5a3a01dd13a68e725347f406f
rahmona-henry <someone@devacademy.co.nz>
2016-02-09 00:12:09 +1300

    Add color

--- main.js
+++ main.js
@@ -92,7 +92,7 @@ $(document).ready(function() {
 
         
         $(document).on('keyup', function () {
-    $('td.active player1_strip').css('background-color', randomColour());
+    $('.racer_table td.active').css('background-color', randomColour());
 
  });
 
